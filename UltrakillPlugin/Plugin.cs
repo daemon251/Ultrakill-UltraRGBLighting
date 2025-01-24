24dacb85b63f164e7d771b128df964052302000c
daemon <51888735+daemon251@users.noreply.github.com>
2025-01-23 22:15:57 -0600

    init upload

---
+++ UltrakillPlugin/Plugin.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+
+using UnityEngine;
+using HarmonyLib;
+using System.Runtime.CompilerServices;
+using System;
+using UnityEngine.SocialPlatforms;
+using System.Linq;
+using UnityEngine.UIElements;
+
+using PluginConfig.API;
+using PluginConfig.API.Decorators;
+using PluginConfig.API.Fields;
+using PluginConfig.API.Functionals;
+
+namespace UltraRGBLighting;
+
+[BepInPlugin("UltraRGBLighting", "UltraRGBLighting", "0.0.1")]
+public class Plugin : BaseUnityPlugin
+{
+    public static int currentRankInt = -1;
+    public static float currentRankFloat = -1f;
+    public float pollingTime = 0.050f; //in seconds
+    string DefaultParentFolder = $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}";
+    string path;
+    private void Awake()
+    {       
+        //Harmony harmony = new Harmony("UltraRGBLighting");
+        //harmony.PatchAll();
+        path = $"{Path.Combine(DefaultParentFolder!, "output.txt")}";
+        var config = PluginConfigurator.Create("UltraRGBLighting", "UltraRGBLighting");
+        config.SetIconWithURL($"{Path.Combine(DefaultParentFolder!, "icon.png")}");
+
+        ConfigHeader infoHeader = new ConfigHeader(config.rootPanel, "Polling rate refers to the amount of times a second that the style rank is recorded. High polling rates have a negative performance impact.");
+        infoHeader.textSize = 12;
+        FloatField pollingRateField = new FloatField(config.rootPanel, "Polling Rate (Hz)", "pollingRate", 30, 0.001f, 1000f);
+        pollingRateField.onValueChange += (FloatField.FloatValueChangeEvent e) => {pollingTime = 1f / e.value;};
+        pollingTime = 1f / pollingRateField.value;
+
+        Debug.Log("UltraRGBLighting.dll Loaded");
+    }
+    float timeElapsed = 0f;
+    void Update()
+    {
+        //All this code does is create a file that has the data that can be read from. I would love for this .dll to use OpenRGB directly,
+        //but it seems that's not possible using Unity because of some version bullshittery. 
+        //regardless, hours of fiddling yielded me no results.
+        //I also don't know how to directly read game variables from outside of a plugin... if thats even possible
+        //thats why the companion app is required.
+
+        //Destructive is zero, etc. No rank is also destructive just not shown.
+        timeElapsed += Time.deltaTime;
+
+        if(MonoSingleton<StyleHUD>.Instance == null) {return;}
+        currentRankInt = MonoSingleton<StyleHUD>.Instance.rankIndex;
+        currentRankFloat = currentRankInt + MonoSingleton<StyleHUD>.Instance.currentMeter / MonoSingleton<StyleHUD>.Instance.currentRank.maxMeter;
+        if(currentRankFloat < 0) {currentRankFloat = -1;} //yes, it does go below zero. This is when no rank is shown at all.
+        //writing to file every frame has a noticeable performance impact, so we need to poll.
+        if(timeElapsed > pollingTime)
+        {
+            File.WriteAllText(path, currentRankFloat.ToString()); 
+            timeElapsed = 0f;
+        }
+    }
+}
+
---
+++ UltrakillPlugin/obj/Debug/net8.0/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace UltraRGBLighting
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "UltraRGBLighting";
+        public const string PLUGIN_NAME = "UltraRGBLighting";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
