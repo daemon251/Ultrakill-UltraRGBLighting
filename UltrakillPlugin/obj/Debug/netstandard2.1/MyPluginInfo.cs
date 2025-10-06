536c822bd9c8501908e67ac6eb37809f5c3e1a42
daemon <51888735+daemon251@users.noreply.github.com>
2025-10-06 17:12:21 -0500

    Delete UltrakillPlugin directory

--- UltrakillPlugin/obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace UltraRGBLighting
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "UltraRGBLighting";
-        public const string PLUGIN_NAME = "UltraRGBLighting";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
